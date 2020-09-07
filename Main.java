41477e29c5af3568cbad7e6f1b61bd13cc88c09d
Abdul Rehman <70048633+AbdulRehman555@users.noreply.github.com>
2020-09-07 12:06:15 +0500

    Interfaces

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String [] args){
+        Book B=new Book();
+        Book Bk=new Book();
+        int result=B.compareTo(Bk);
+        System.out.println(result);
+
+    }
+}
